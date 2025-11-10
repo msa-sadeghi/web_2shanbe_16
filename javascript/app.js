77fb5a3d22300a66c730f4172417c1abb90abea3
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-11-10 16:14:37 +0330

    s

--- javascript/app.js
+++
@@ -1,7 +0,0 @@
-let divElem = document.getElementById("welcomeMessage")
-
-function showWelcome(){
-    let userName = prompt("اسمتون رو بفرمائید")
-    divElem.textContent = `سلام ${userName}`
-    divElem.style.display =  "block"
-}
