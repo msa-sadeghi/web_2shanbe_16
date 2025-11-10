cc356006998bfc4b9bb3c6364739e0f5714237d5
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-11-10 16:27:26 +0330

    s

---
+++ javascript/app.js
@@ -0,0 +1,39 @@
+let tempInput = document.getElementById("tempInput")
+let tempFrom = document.getElementById("tempFrom")
+let tempTo = document.getElementById("tempTo")
+let tempResult = document.getElementById("tempResult")
+
+
+function convertTemperature(){
+    let tempInputValue = tempInput.value
+    let tempFromValue = tempFrom.value
+    let tempToValue = tempTo.value
+    let result
+
+    if(tempFromValue == tempToValue){
+        result = tempInputValue
+    }else if(tempFromValue == "fahrenheit"){
+        if(tempToValue == "celsius"){
+            result = tempInputValue * 2
+        }else if (tempToValue == "kelvin"){
+            result = tempInputValue * 3
+        }
+    }else if(tempFromValue == "celsius"){
+        if(tempToValue == "fahrenheit"){
+            result = tempInputValue * 2
+        }else if (tempToValue == "kelvin"){
+            result = tempInputValue * 3
+        }
+    }
+    else if(tempFromValue == "kelvin"){
+        if(tempToValue == "fahrenheit"){
+            result = tempInputValue * 2
+        }else if (tempToValue == "celsius"){
+            result = tempInputValue * 3
+        }
+    }
+
+    tempResult.innerHTML = result
+    tempResult.style.display = "block"
+
+}
