ad7efa07d796e06716766ef6e42b1c9872e84266
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-11-17 16:57:39 +0330

    s

--- javascript/app.js
+++ javascript/app.js
@@ -1,39 +1,65 @@
-let tempInput = document.getElementById("tempInput")
-let tempFrom = document.getElementById("tempFrom")
-let tempTo = document.getElementById("tempTo")
-let tempResult = document.getElementById("tempResult")
+// let score = 0
+// score = score + 1
+// console.log(score)
+// score += 1
+// console.log(score)
+// score++
+// console.log(score)
+
+// console.log("123" === 123)
+
+//   &&    ||    !
 
 
-function convertTemperature(){
-    let tempInputValue = tempInput.value
-    let tempFromValue = tempFrom.value
-    let tempToValue = tempTo.value
-    let result
 
-    if(tempFromValue == tempToValue){
-        result = tempInputValue
-    }else if(tempFromValue == "fahrenheit"){
-        if(tempToValue == "celsius"){
-            result = tempInputValue * 2
-        }else if (tempToValue == "kelvin"){
-            result = tempInputValue * 3
-        }
-    }else if(tempFromValue == "celsius"){
-        if(tempToValue == "fahrenheit"){
-            result = tempInputValue * 2
-        }else if (tempToValue == "kelvin"){
-            result = tempInputValue * 3
-        }
-    }
-    else if(tempFromValue == "kelvin"){
-        if(tempToValue == "fahrenheit"){
-            result = tempInputValue * 2
-        }else if (tempToValue == "celsius"){
-            result = tempInputValue * 3
-        }
-    }
+// let isLoggedIn = false
+// isLoggedIn ? document.body.innerHTML = "<h1>Hello user</h1>" :
+//             document.body.innerHTML = "<h1>Not Allowed</h1>"
+// let  day = 1
+// switch(day){
+//     case 1:
+//         console.log("mon")
+//         break
+//     case 2:
+//         console.log("tue")
+//         break
 
-    tempResult.innerHTML = result
-    tempResult.style.display = "block"
+//     default:
+//         console.log("blalalal")
+// }
+// for(let i = 0; i < 5; i++){
+//     console.log(i)
+// }
+// let j = 0
+// while(j < 5){
+//     console.log(j)
+//     j++
+// }
+// let z = 1
+// do{
+//     console.log(z)
+//     z++
+// }
+// while(z < 5)
 
+let numbers = [1, 2, 3, 4, 5]
+let s = 0
+numbers.push(6)
+for(let i = 0; i < numbers.length; i++){
+    s += numbers[i]
 }
+console.log(s)
+
+s = 0
+numbers.forEach(function(n){
+    s += n
+})
+console.log(s)
+
+let fruits = ["apple", "banana", "grape"]
+let res = "<ul>"
+fruits.forEach(function(item){
+    res += "<li>" + item + "</li>" 
+})
+res += "</ul>"
+document.body.innerHTML =  res
