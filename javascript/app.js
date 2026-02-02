c09731779b675ec609c7153806a46123e70eae49
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-02-02 16:37:01 +0330

    s

--- javascript/app.js
+++ javascript/app.js
@@ -1,28 +1,26 @@
-let form = document.getElementById("myForm")
-let name = document.getElementById("name")
-let email = document.getElementById("email")
-let age = document.getElementById("age")
-let country = document.getElementById("country")
-let info = document.getElementById("info")
-let submitBtn = document.querySelector("button")
+const container = document.querySelector('.container')
 
+function getAllUsers(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(r => r.json())
+    .then(data => renderUsers(data))
+    .catch(error=>console.log(error))
+}
 
-name.addEventListener("input", (e)=>{
-    let l = e.target.value.length
-    let nameError = document.getElementById("nameError")
-    if(l < 3){
-        nameError.style.display = "block"
-        name.classList.add("error")
-    }else{
-        nameError.style.display = "none"
-        name.classList.remove("error")
-    }
-})
+getAllUsers()
 
-email.addEventListener("input", (e)=>{
-    let emailError = document.getElementById("emailError")
-    let emailVal = e.target.value
-    // let pattern =  /^[\w\._]+@[a-zA-Z]{2,}\.[a-zA-Z]{2,}/
-    // pattern.test(emailVal)
+function renderUsers(users){
+    users.forEach(user => {
+        const userCard = document.createElement("div")
+        userCard.classList.add('card')
+        const userName = document.createElement("h3")
+        userName.innerText = user.name
+        const userIdSpan = document.createElement('span')
+        userIdSpan.innerText = user.id
+        const usenameSpan = document.createElement('span')
+        usenameSpan.innerText = user.username
 
-})
+        userCard.append(userName, userIdSpan, usenameSpan)
+        container.append(userCard)
+    });
+}
